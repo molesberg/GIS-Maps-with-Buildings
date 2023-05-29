22f06d257429674b7290f0d62788bfabaec13ec8
molesberg <michael.olesberg@gmail.com>
2023-05-29 13:22:45 -0400

    Identify combos

--- GISpractice.py
+++ GISpractice.py
@@ -2,34 +2,44 @@ import arcpy
 import numpy as np
 
 # Set the workspace environment
-gdb = r".gdb"
+gdb = r"D:\ArcGIS Pro\Projects\Unclass build\Localtest.gdb"
 arcpy.env.workspace = gdb
 
 # List all feature classes within the "PYLON" feature dataset
-feature_classes = arcpy.ListFeatureClasses(feature_dataset="PYLON")
+feature_classes = arcpy.ListFeatureClasses(feature_dataset="Portaldataoffline")
 
-# Define the fields to consider
-fields = ["COMPOUND", "STRUCTURE", "FLOOR"]
+def getcombos():
+    # Define the fields to consider
+    fields = ["COMPOUND", "STRUCTURE", "FLOOR"]
 
-# Create an empty list to store unique combinations
-unique_combinations = []
+    # Create an empty list to store unique combinations
+    combos = []
 
-# Loop through each feature class
-for fc in feature_classes:
-    # Get the field names present in the feature class
-    field_names = [field.name for field in arcpy.ListFields(fc)]
-    
-    # Check if all the fields are present in the feature class
-    if all(field in field_names for field in fields):
-        # Retrieve the field values using a numpy array
-        values = arcpy.da.TableToNumPyArray(fc, fields)
+    # Loop through each feature class
+    for fc in feature_classes:
+        # Get the field names present in the feature class
+        field_names = [field.name for field in arcpy.ListFields(fc)]
         
-        # Find unique combinations of the fields using numpy.unique
-        combinations = np.unique(values)
-        
-        # Append the combinations to the unique_combinations list
-        unique_combinations.extend(combinations.tolist())
+        # Check if all the fields are present in the feature class
+        if all(field in field_names for field in fields):
+            # Retrieve the field values using a numpy array
+            values = arcpy.da.TableToNumPyArray(fc, fields)
+            
+            # Find unique combinations of the fields using numpy.unique
+            combinations = np.unique(values)
+            
+            # Append the combinations to the combos list
+            combos.extend(combinations.tolist())
 
+    # M: Isolate overlap between features
+    combos =  np.unique(combos, axis = 0)
+    return combos
+
+combos = getcombos()
 # Print the unique combinations
-for combination in unique_combinations:
+for combination in combos:
     print(combination)
+
+#list of unique compounds
+comp = np.unique(combos[:, 0])
+print('\n', comp)
